456098794f7cd18c5b73c98e99d6a6b3de1ea201
kimsavinfo <kimsavinfo@gmail.com>
2014-06-23 16:59:30 +0200

    Début Factory

--- src/FlyWeight/FlyWeightFactory.java
+++ src/FlyWeight/FlyWeightFactory.java
@@ -1,13 +1,29 @@
 package FlyWeight;
 
+import FlyWeight.Composite.OpAddition;
+import FlyWeight.Composite.OpComposite;
+import Library.Operateurs;
+
+import java.util.HashMap;
+
 /**
  * Created by kimsavinfo on 22/06/14.
  */
 public class FlyWeightFactory
 {
-    public void construire(exp A)
-    {
+    HashMap<Double, OpNombre> nombresGeneres;
+    HashMap<String, OpComposite> operateursGeneres;
+    Operateurs operateursAcceptes;
+    // Sujet ne demande qu'une varaible "x" mais on anticipe l'évolution
+    // d'une équation à 2 inconnues par exemple
+    HashMap<String, OpVariable> variablesGenerees;
 
+    public FlyWeightFactory()
+    {
+        nombresGeneres = new HashMap<Double, OpNombre>();
+        operateursGeneres = new HashMap<String, OpComposite>();
+        operateursAcceptes = new Operateurs();
+        variablesGenerees = new HashMap<String, OpVariable>();
     }
 
     public void construire(signe, expA, expB)
@@ -15,9 +31,68 @@ public class FlyWeightFactory
 
     }
 
-    public void construireNombre()
+    public OpNombre construire(Double p_nombre)
+    {
+        OpNombre opNombre = null;
+
+        if(!nombresGeneres.containsKey(p_nombre))
+        {
+            opNombre = new OpNombre(p_nombre);
+            nombresGeneres.put(p_nombre, opNombre);
+        }
+        else
+        {
+            opNombre = nombresGeneres.get(p_nombre);
+        }
+
+        return opNombre;
+    }
+
+
+    public OpVariable construire(String p_expression)
+    {
+        OpVariable opVariable = null;
+
+        if(!variablesGenerees.containsKey(p_variable))
+        {
+            opVariable = new OpVariable(p_variable);
+            variablesGenerees.put(p_variable, opVariable);
+        }
+        else
+        {
+            opVariable = variablesGenerees.get(p_variable);
+        }
+
+        return opVariable;
+    }
+
+    private OpComposite construire(String p_expression)
+    {
+        OpComposite opComposite = null;
+
+        if(!operateursGeneres.containsKey(p_expression))
+        {
+            opComposite = new OpComposite(p_expression);
+            nombresGeneres.put(p_nombre, opNombre);
+        }
+        else
+        {
+            opNombre = nombresGeneres.get(p_nombre);
+        }
+
+        switch (p_expression)
+        {
+            case "+":
+
+            break;
+        }
+    }
+
+    private OpAddition construireOpAddition()
     {
 
     }
+
+
 
 }
---
+++ src/Library/Operateurs.java
@@ -0,0 +1,17 @@
+package Library;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kimsavinfo on 23/06/14.
+ */
+public class Operateurs
+{
+    List<String> operateurs = Arrays.asList("+", "-!", "/", "*");
+
+    public List<String>  getOperateurs()
+    {
+        return operateurs;
+    }
+}
